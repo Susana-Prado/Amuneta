ff29b0d0ae4b37ee0714d75edf77798a9c207c23
Susana Prado <susana.prado01@gmail.com>
2021-06-02 18:22:32 +0200

    models done & general configs

---
+++ amuneta-server/app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+const app = express();
+
+//DB config
+require('./configs/db.config');
+
+//Middleware config
+require('./configs/middleware.config')(app);
+require('./configs/cors.config')(app);
+
+// Session config + Passport
+require('./configs/session.config')(app);
+require('./configs/passport.config')(app);
+
+
+
+
+//Routes
+const index = require('./routes/index');
+app.use('/', index);
+
+
+app.use((req, res, next) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({ message: "Not found"});
+})
+
+
+module.exports = app;
